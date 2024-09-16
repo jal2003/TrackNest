b35b4c16573110acec00c84b24128a34f7cf68e5
Jal Desai <jaldesai715@gmail.com>
2024-09-16 14:40:29 -0400

    minor bugs

--- backend/typeDefs/index.js
+++ backend/typeDefs/index.js
@@ -1,10 +1,9 @@
 import { mergeTypeDefs } from "@graphql-tools/merge";
 
-//TypeDefs
+// typeDefs
 import userTypeDef from "./user.typeDef.js";
 import transactionTypeDef from "./transaction.typeDef.js";
 
 const mergedTypeDefs = mergeTypeDefs([userTypeDef, transactionTypeDef]);
 
 export default mergedTypeDefs;
-
--- backend/typeDefs/user.typeDef.js
+++ backend/typeDefs/user.typeDef.js
@@ -4,37 +4,37 @@ const userTypeDef = `#graphql
     username: String!
     name: String!
     password: String!
-    profilePic: String
+    profilePicture: String
     gender: String!
- }
+    transactions: [Transaction!]
+  }
 
   type Query {
-    users: [User!]
     authUser: User
-    user(userId: ID!): User
- }
+    user(userId:ID!): User
+  }
 
   type Mutation {
     signUp(input: SignUpInput!): User
     login(input: LoginInput!): User
     logout: LogoutResponse
- }
+  }
 
   input SignUpInput {
     username: String!
     name: String!
     password: String!
     gender: String!
-    }
+  }
 
   input LoginInput {
     username: String!
     password: String!
- }
+  }
 
   type LogoutResponse {
     message: String!
-    }
+  }
+`;
 
-`
-export default userTypeDef
+export default userTypeDef;
